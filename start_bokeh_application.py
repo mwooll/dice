fdeaa0298bc933b7f9cee7deeab6cc7f76af762c
mwooll <mark.woolley@uzh.ch>
2022-05-31 09:55:54 +0200

    removing not working files, might get back to to do them from scratch

--- start_bokeh_application.py
+++
@@ -1,6 +0,0 @@
-# Alakazam's_Trickery
-
-import subprocess
-
-command = "start python -m bokeh serve --show dice_visualisation.py"
-process = subprocess.Popen(command, shell=True, start_new_session=True).wait()
